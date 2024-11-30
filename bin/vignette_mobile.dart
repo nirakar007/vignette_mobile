6a6a43b9f156c98906744be235b7c10aabeeac79
nirakar007 <neerakarsuwal106@gmail.com>
2024-12-01 01:28:40 +0545

    first commit

---
+++ bin/vignette_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:vignette_mobile/vignette_mobile.dart' as vignette_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vignette_mobile.calculate()}!');
+}
---
+++ test/vignette_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:vignette_mobile/vignette_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
